8b57e27d46aa216e8d858a94c35de2b0cedc4b6f
bkt-tonton <goneisgone@hotmail.com>
2022-04-20 10:28:44 -0300

    Angular Reddit Stage3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[]; //<== component property 
+    //The Article[] might look a little unfamiliar. We’re saying here that articles is an
+    //Array of Articles.
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('FullStack', 'http://fullstack.io', 3),
+      new Article('Angular Homepage', 'http://angular.io', 3),
+    ]
+
+
+  }
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,36 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+
+  article: Article;
+
+
+  constructor() {
+    this.article = new Article(
+      'Angular',
+      'http://angular.io',
+      10
+    );
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown()
+    return false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
